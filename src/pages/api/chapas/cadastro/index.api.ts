2d36cc8e8576bbc3c1b2bb8ebd465ff39fecb8be
Gui Monteiro <guilhermemonteiroro@gmail.com>
2024-01-07 10:55:23 -0300

    feat: routes api

---
+++ src/pages/api/chapas/cadastro/index.api.ts
@@ -0,0 +1,21 @@
+import { prisma } from '@/lib/prisma'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { z } from 'zod'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const data = req.body
+
+  try {
+    await prisma.chapas.create({
+      ...data,
+    })
+    return res.status(201).end()
+  } catch (error) {
+    console.log(error)
+    const ErrorMessage = `Error conect database`
+    return res.status(500).json({ message: `${ErrorMessage}` })
+  }
+}
