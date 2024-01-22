f5ee280a2d37c3c577a1837ef78d57cb87f6b4bc
DiegoPedroso <diego.pedroso15@gmail.com>
2024-01-21 21:42:03 -0300

    feat: :sparkles: add new verification and fix perfomance

--- src/pages/api/importExcelPagamentos/index.api.ts
+++ src/pages/api/importExcelPagamentos/index.api.ts
@@ -1,8 +1,9 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import formidable, { File, IncomingForm } from 'formidable';
-import fs from 'fs';
-import Papa from 'papaparse';
-import { prisma } from '@/lib/prisma'; // Ajuste o caminho conforme necessário
+import { NextApiRequest, NextApiResponse } from "next";
+import formidable, { File, IncomingForm } from "formidable";
+import fs from "fs";
+import Papa from "papaparse";
+import { prisma } from "@/lib/prisma"; // Ajuste o caminho conforme necessário
+import { Logs } from "@/utils/Logs";
 
 export const config = {
   api: {
@@ -10,9 +11,12 @@ export const config = {
   },
 };
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST') {
-    res.status(405).json({ message: 'Somente método POST permitido' });
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "POST") {
+    res.status(405).json({ message: "Somente método POST permitido" });
     return;
   }
 
@@ -20,42 +24,68 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   form.parse(req, async (err, fields, files) => {
     if (err) {
-      res.status(500).json({ message: 'Erro ao processar o arquivo' });
+      res.status(500).json({ message: "Erro ao processar o arquivo" });
       return;
     }
 
     if (!files.file || !Array.isArray(files.file) || files.file.length === 0) {
-      res.status(400).json({ message: 'Nenhum arquivo foi enviado.' });
+      res.status(400).json({ message: "Nenhum arquivo foi enviado." });
       return;
-  }
+    }
 
-  const file = files.file[0] as formidable.File;
+    const file = files.file[0] as formidable.File;
+
+    if (!file.originalFilename || !file.originalFilename.endsWith('.csv')) {
+      res.status(400).json({ message: 'Por favor, envie um arquivo .csv.' });
+      return;
+    }
+
     const csvFilePath = file.filepath;
-    const csvData = fs.readFileSync(csvFilePath, 'utf8');
+    const csvData = fs.readFileSync(csvFilePath, "utf8");
     const records = Papa.parse(csvData, {
       header: true,
-      delimiter: ';',
+      delimiter: ";",
       skipEmptyLines: true,
     }).data;
 
     for (const record of records as any[]) {
-      await prisma.pagamentos.create({
-        data: {
-          matricula_saerj: record.customer_id,
-          tipo_pagamento: record.payment_method,
-          ano_anuidade: new Date(record.order_date).getFullYear().toString(),
-          data_pagto_unica: record.paid_date ? new Date(record.paid_date).toISOString().split('T')[0] : '',
-          valor_pagto_unica: record.order_total.toString(),
-          data_pagto_parcela_1: '',
-          valor_pagto_parcela_1: '',
-          data_pagto_parcela_2: '',
-          valor_pagto_parcela_2: '',
-          data_pagto_parcela_3: '',
-          valor_pagto_parcela_3: '',
-        },
-      });
+      if (record.status && record.status.toLowerCase() === "paid") {
+
+        if (record.customer_note && record.customer_note.startsWith("CPF TITULAR: ")) {
+          const cpf = record.customer_note.substring("CPF TITULAR: ".length).trim();
+
+          const associado = await prisma.associados.findFirst({
+            where: {
+              cpf: cpf,
+            },
+          });
+    
+          if (associado?.matricula_SAERJ) {
+            await prisma.pagamentos.create({
+              data: {
+                matricula_saerj: typeof Number(associado.matricula_SAERJ) ?? 0,
+                tipo_pagamento: record.payment_method ?? "",
+                ano_anuidade: new Date(record.order_date).getFullYear().toString(),
+                data_pagto_unica: record.order_date ? new Date(record.order_date).toISOString().split('T')[0] : '',
+                valor_pagto_unica: record.order_total ? record.order_total.toString() : '',
+                data_pagto_parcela_1: "",
+                valor_pagto_parcela_1: "",
+                data_pagto_parcela_2: "",
+                valor_pagto_parcela_2: "",
+                data_pagto_parcela_3: "",
+                valor_pagto_parcela_3: "",
+              },
+            });
+          } else {
+            Logs({
+                modulo: "Pagamentos",
+                descriptionLog: `CPF ${cpf} não encontrado na base de associados.`,
+              })
+          }
+        }
+      }
     }
 
-    res.status(200).json({ message: 'Dados do CSV importados com sucesso!' });
+    res.status(200).json({ message: "Dados do CSV importados com sucesso!" });
   });
 }
