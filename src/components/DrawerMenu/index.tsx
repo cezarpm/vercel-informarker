e7fc6aa9a9bd0dfba8eadcf9ef0fcade7a8b09be
cezarpm <cezar_pm@hotmail.com>
2024-02-14 12:50:47 -0300

    icones

--- src/components/DrawerMenu/index.tsx
+++ src/components/DrawerMenu/index.tsx
@@ -1,15 +1,15 @@
-import * as React from 'react'
-import Box from '@mui/material/Box'
-import Drawer from '@mui/material/Drawer'
-import Button from '@mui/material/Button'
-import List from '@mui/material/List'
-import Divider from '@mui/material/Divider'
-import ListItem from '@mui/material/ListItem'
-import ListItemButton from '@mui/material/ListItemButton'
-import ListItemIcon from '@mui/material/ListItemIcon'
-import ListItemText from '@mui/material/ListItemText'
-import Image from 'next/image'
-import logo from '@/assets/logo.png'
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Image from "next/image";
+import logo from "@/assets/logo.png";
 import {
   Buildings,
   CloudArrowUp,
@@ -24,66 +24,69 @@ import {
   User,
   ArchiveBox,
   Money,
-} from 'phosphor-react'
-import { useRouter } from 'next/navigation'
-import { Twirl as Hamburger } from 'hamburger-react'
-type Anchor = 'left'
+} from "phosphor-react";
+import { useRouter } from "next/navigation";
+import { Twirl as Hamburger } from "hamburger-react";
+type Anchor = "left";
 
 export function TemporaryDrawer() {
   const [state, setState] = React.useState({
     left: false,
-  })
+  });
 
-  const [isOpen, setOpen] = React.useState(false)
+  const [isOpen, setOpen] = React.useState(false);
 
-  const router = useRouter()
+  const router = useRouter();
 
-  // const { Logs } = useContextCustom()
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    router.push("/auth");
+  };
 
   function handleNextPage(text: string): void {
-    if (text === 'Parametros') {
-      router.push('/parametros')
-    } else if (text === 'Empresas') {
-      router.push('/empresas')
-    } else if (text === 'Tabelas') {
-      router.push('/tabelas')
-    } else if (text === 'Associados') {
-      router.push('/associados/cadastro')
-    } else if (text === 'Logs') {
-      router.push('/logs')
-    } else if (text === 'Aniversariantes') {
-      router.push('/associados/aniversariantes')
-    } else if (text === 'Protocolos') {
-      router.push('/protocolos')
-    } else if (text === 'Chapas') {
-      router.push('/chapas')
-    } else if (text === 'Diretorias') {
-      router.push('/diretorias')
-    } else if (text === 'Eleições') {
-      router.push('/eleicao/lista')
-    } else if (text === 'Votação') {
-      router.push('/eleicao')
-    } else if (text === 'Importação anuidades') {
-      router.push('/importacaoPagamentos')
-    } else if (text === 'Pagamentos') {
-      router.push('/pagamentos')
+    if (text === "Parametros") {
+      router.push("/parametros");
+    } else if (text === "Empresas") {
+      router.push("/empresas");
+    } else if (text === "Tabelas") {
+      router.push("/tabelas");
+    } else if (text === "Associados") {
+      router.push("/associados/cadastro");
+    } else if (text === "Logs") {
+      router.push("/logs");
+    } else if (text === "Aniversariantes") {
+      router.push("/associados/aniversariantes");
+    } else if (text === "Protocolos") {
+      router.push("/protocolos");
+    } else if (text === "Chapas") {
+      router.push("/chapas");
+    } else if (text === "Diretorias") {
+      router.push("/diretorias");
+    } else if (text === "Eleições") {
+      router.push("/eleicao/lista");
+    } else if (text === "Votação") {
+      router.push("/eleicao");
+    } else if (text === "Importação anuidades") {
+      router.push("/importacaoPagamentos");
+    } else if (text === "Pagamentos") {
+      router.push("/pagamentos");
     }
   }
 
   const toggleDrawer =
     (anchor: Anchor, open: boolean) =>
-      (event: React.KeyboardEvent | React.MouseEvent) => {
-        if (
-          event.type === 'keydown' &&
-          ((event as React.KeyboardEvent).key === 'Tab' ||
-            (event as React.KeyboardEvent).key === 'Shift')
-        ) {
-          return
-        }
-
-        setState({ ...state, [anchor]: open })
-        setOpen(open)
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
       }
+
+      setState({ ...state, [anchor]: open });
+      setOpen(open);
+    };
 
   const arrayIcons = [
     <>
@@ -116,7 +119,7 @@ export function TemporaryDrawer() {
     <>
       <User size={30} color="#fff" />
     </>,
-  ]
+  ];
 
   const arrayIconsTwoList = [
     <>
@@ -128,7 +131,7 @@ export function TemporaryDrawer() {
     <>
       <Function size={30} color="#fff" />
     </>,
-  ]
+  ];
 
   const list = (anchor: Anchor) => (
     <Box
@@ -138,34 +141,32 @@ export function TemporaryDrawer() {
       onKeyDown={toggleDrawer(anchor, false)}
       //   bgcolor={'linear-gradient(180deg, #0DA9A4 45%, #00F2FF 89%)'}
       style={{
-        height: '100%',
-        background: 'linear-gradient(180deg, #0DA9A4 45%, #00F2FF 89%)',
+        height: "100%",
+        background: "linear-gradient(180deg, #0DA9A4 45%, #00F2FF 89%)",
       }}
     >
       <div
         style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          width: '100%',
-          padding: '2rem 0rem',
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "100%",
+          padding: "2rem 0rem",
         }}
       >
         <Image src={logo} alt="logo" width={210} quality={100} />
       </div>
 
       <List>
-        {[
-          'Associados',
-        ].map((text, index) => (
+        {["Associados"].map((text, index) => (
           <ListItem key={text} disablePadding>
             <ListItemButton
               onClick={() => {
-                handleNextPage(text)
+                handleNextPage(text);
               }}
             >
               <ListItemIcon>{arrayIcons[index]}</ListItemIcon>
-              <ListItemText primary={text} style={{ color: '#fff' }} />
+              <ListItemText primary={text} style={{ color: "#fff" }} />
             </ListItemButton>
           </ListItem>
         ))}
@@ -187,14 +188,14 @@ export function TemporaryDrawer() {
         ))}
       </List> */}
     </Box>
-  )
+  );
 
   return (
     <div>
-      {(['left'] as const).map((anchor) => (
+      {(["left"] as const).map((anchor) => (
         <React.Fragment key={anchor}>
           <Button
-            style={{ color: '#fff' }}
+            style={{ color: "#fff" }}
             onClick={toggleDrawer(anchor, true)}
           >
             <Hamburger toggled={isOpen} />
@@ -207,19 +208,24 @@ export function TemporaryDrawer() {
             {list(anchor)}
             <div
               style={{
-                backgroundColor: '#00F2FF',
-                padding: '8px 16px',
-                display: 'flex',
-                alignItems: 'center',
-                gap: '0.5rem',
+                backgroundColor: "#00F2FF",
+                padding: "8px 16px",
+                display: "flex",
+                alignItems: "center",
+                gap: "0.5rem",
               }}
             >
               <SignOut size={30} color="red" />
-              <span style={{ color: 'red', fontWeight: 'bold' }}>Sair</span>
+              <span
+                onClick={handleLogout}
+                style={{ color: "red", fontWeight: "bold" }}
+              >
+                Sair
+              </span>
             </div>
           </Drawer>
         </React.Fragment>
       ))}
     </div>
-  )
+  );
 }
--- src/pages/auth/styled.ts
+++ src/pages/auth/styled.ts
@@ -1,31 +1,32 @@
-import { styled } from '@ignite-ui/react'
+import { styled } from "@ignite-ui/react";
 
-export const Container = styled('main', {
-    width: '700px',
-    margin: '10% auto',
-    padding: '20px',
-    display: 'flex',
-    flexDirection: 'column',
-    gap: '20px',
+export const Container = styled("main", {
+  maxWidth: "700px",
+  margin: "10% auto",
+  padding: "20px",
+  display: "flex",
+  flexDirection: "column",
+  gap: "20px",
 
-    '> form': {
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '20px',
-    }
-})
+  "> form": {
+    display: "flex",
+    flexDirection: "column",
+    gap: "20px",
+  },
+});
 
-export const Title = styled('h1', {
-    fontSize: '24px',
-    color: 'rgba(0, 0, 0, 1)',
-    textAlign: 'center',
-})
+export const Title = styled("h1", {
+  fontSize: "18px",
+  color: "rgba(0, 0, 0, 1)",
+  textAlign: "center",
+  fontFamily: "Roboto",
+});
 
-export const Description = styled('p', {
-    fontSize: '16px',
-    color: 'rgba(0, 0, 0, 1)',
-    textAlign: 'center',
-    marginBottom: '20px',
-})
-
-
+export const Description = styled("p", {
+  fontSize: "12px",
+  color: "rgba(0, 0, 0, 1)",
+  textAlign: "center",
+  marginBottom: "20px",
+  fontFamily: "Roboto",
+  marginTop: "10px",
+});
